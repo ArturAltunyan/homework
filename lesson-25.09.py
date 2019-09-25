7193ae4db9f0e43c3a14505c93de39e0f720991f
ArturAltunyan <arturaltunyan@gmail.com>
2019-09-25 21:23:19 +0400

    Else , elif

---
+++ lesson-25.09.py
@@ -0,0 +1,17 @@
+
+
+input_temperature = int(input("Temperature: "))
+if input_temperature <= 10:
+	print("wear cloth")
+elif input_temperature > 10 and input_temperature < 35:
+	print("wear t-shirt")
+else:
+	print("Keep calm")
+
+
+#input_weather = int(input("weather: "))
+#if input_weather <= 10:
+	#print("cold", input_weather)
+#else:
+	#print("hot", input_weather)git status
+	
