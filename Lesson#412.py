39354102e4da092009f177eb32587c9da53a0a47
ArturAltunyan <arturaltunyan@gmail.com>
2019-09-27 21:20:44 +0400

    Parz tiv

---
+++ Lesson#412.py
@@ -0,0 +1,6 @@
+input_user = int(input("Any number:"))
+
+for i in range(0, input_user):
+	for j in range(1, input_user):
+		if input_user / i == input_user and input_user / i == 1 and input_user != input_user:
+			print(i)
