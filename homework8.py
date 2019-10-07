b57c93a53b9a0158d253cb74153b8ba9bdcd7b0d
ArturAltunyan <arturaltunyan@gmail.com>
2019-10-07 21:13:28 +0400

    string methods, replace, capitalize and etc.

---
+++ homework8.py
@@ -0,0 +1,17 @@
+
+# a = "jhfowhfo"
+# for char in a:
+# 	print(ord(char))
+# b = ord('a')
+# print(b / 2)
+# print(chr(33))
+a = "skfnwef"
+print(a[2:-1])
+print(a.find("skf"))
+my_string = "this is MY test string!"
+print(my_string)
+print(my_string.capitalize())
+print(my_string.lower()) 
+print(my_string.upper())
+print(my_string.title())
+print(my_string.replace("MY", "YOUR"))       
