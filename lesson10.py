fd59d991e292fd1618d78c50ddc3455699215ad6
ArturAltunyan <arturaltunyan@gmail.com>
2019-10-09 19:50:58 +0400

    Tuple, Nesting tules

---
+++ lesson10.py
@@ -0,0 +1,15 @@
+a = (1,2)
+
+print(a[0], a[1])
+
+def tuple1():
+	user_name = input("enter your name")
+	user_surname = input("Enter your surname")
+	return user_name, user_surname
+user_info = tuple1()
+print("Your name is " + user_info[0])
+print("Your surname is " + user_info[1])
+
+nested_tuple = ((1, 2), (3, 4), (5, 6))
+print(nested_tuple[0])
+print(nested_tuple[0][0])
