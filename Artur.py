c434d2ebad41aea2cee08d56ae626547cb2700c5
ArturAltunyan <arturaltunyan@gmail.com>
2019-10-11 21:12:56 +0400

    file write, read, apperde and examples

--- Artur.py
+++ Artur.py
@@ -37,11 +37,29 @@
 # out_put_file.close()
 
 
-name_list =[]
-out_put_file = open("outputfile.txt", "r")
+# name_list =[]
+# out_put_file = open("outputfile.txt", "r")
 
-for line in out_put_file:
-	name_list.append(line)
-out_put_file.close()
-print(name_list)
+# for line in out_put_file:
+# 	name_list.append(line)
+# out_put_file.close()
+# print(name_list)
 
+
+# def new_list(list, n):
+# 	new_list=[]
+# 	for i in list:
+# 		if len(i) > n:
+# 			new_list.append(i)
+# 	return new_list
+# my_list = ["pop", "scas", "jwioshiw", "ggfs"]
+
+# print(new_list(my_list, 3))
+
+user_list =[]
+input_user = input("Write some Words:")
+
+for i in input_user:
+ 	if len(i)%2 == 0:
+ 		print(user_list.append(i))
+ 		
