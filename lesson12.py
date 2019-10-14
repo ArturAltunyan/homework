10321882d19c4e1ab30c6f2e93719e87181565ae
ArturAltunyan <arturaltunyan@gmail.com>
2019-10-14 19:50:30 +0400

    change and del in dictionary

--- lesson12.py
+++ lesson12.py
@@ -1,3 +1,6 @@
 human = {"name": "Artur", "surname": "Altunyan", "Age": 36}
 print(human)
 print(human["name"], human["surname"], human["Age"])
+human["have_pet"] = True
+del human["Age"]
+print(human)
