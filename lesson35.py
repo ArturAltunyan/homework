c82695f7778beefac1550789bea3de3a6c9847e0
ArturAltunyan <arturaltunyan@gmail.com>
2019-10-28 21:08:37 +0400

    test28.10.2019

---
+++ lesson35.py
@@ -0,0 +1,44 @@
+a = "Oh yeah, I'll tell you somethin"
+b = "I think you'll understand"
+c = "When I say that somethin"
+d = "I want to hold your hand"
+e = "I want to hold your hand"
+f = "I want to hold your hand"
+g = "Oh please, say to me"
+h = "You'll let me be your man"
+i = "And please, say to me"
+j = "You'll let me hold your hand"
+k = "Now, let me hold your hand"
+l = "And when I touch you"
+m = "I feel happy inside"
+n = "It's such a feelin' that my love"
+o = "I can't hide"
+p = "I can't hide"
+q = "I can't hide"
+r = "Yeah, you got that somethin"
+s = "I think you'll understand"
+t = "When I say that somethin"
+u = "I want to hold your hand"
+v = "I want to hold your hand"
+w = "I want to hold your hand"
+x = "And when I touch you"
+y = "I feel happy inside"
+z = "It's such a feelin' that my love"
+aa = "I can't hide"
+bb = "I can't hide"
+cc = "I can't hide"
+dd = "Yeah, you got that somethin"
+ee = "I think you'll understand"
+ff = "When I feel that somethin"
+gg = "I want to hold your hand"
+hh = "I want to hold your hand"
+ii = "I want to hold your hand"
+jj = "I want to hold your hand"
+
+
+
+b = input("write any number")
+for i in
+dic = {b: a}
+dic["first"]
+print(dic["first"])
