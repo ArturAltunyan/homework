1672541a0a87675976a1fa4520965b85193082ad
ArturAltunyan <arturaltunyan@gmail.com>
2019-10-28 21:23:56 +0400

    test 28.10.2019

--- lesson35.py
+++ lesson35.py
@@ -1,44 +1,45 @@
-a = "Oh yeah, I'll tell you somethin"
-b = "I think you'll understand"
-c = "When I say that somethin"
-d = "I want to hold your hand"
-e = "I want to hold your hand"
-f = "I want to hold your hand"
-g = "Oh please, say to me"
-h = "You'll let me be your man"
-i = "And please, say to me"
-j = "You'll let me hold your hand"
-k = "Now, let me hold your hand"
-l = "And when I touch you"
-m = "I feel happy inside"
-n = "It's such a feelin' that my love"
-o = "I can't hide"
-p = "I can't hide"
-q = "I can't hide"
-r = "Yeah, you got that somethin"
-s = "I think you'll understand"
-t = "When I say that somethin"
-u = "I want to hold your hand"
-v = "I want to hold your hand"
-w = "I want to hold your hand"
-x = "And when I touch you"
-y = "I feel happy inside"
-z = "It's such a feelin' that my love"
-aa = "I can't hide"
-bb = "I can't hide"
-cc = "I can't hide"
-dd = "Yeah, you got that somethin"
-ee = "I think you'll understand"
-ff = "When I feel that somethin"
-gg = "I want to hold your hand"
-hh = "I want to hold your hand"
-ii = "I want to hold your hand"
-jj = "I want to hold your hand"
+dic= {1: "Oh yeah, I'll tell you somethin",
+		2 : "I think you'll understand",
+		3 : "When I say that somethin",
+		4 : "I want to hold your hand",
+		5 : "I want to hold your hand",
+		6 : "I want to hold your hand",
+		7 : "Oh please, say to me",
+		8 : "You'll let me be your man",
+		9 : "And please, say to me",
+		10 : "You'll let me hold your hand",
+		11 : "Now, let me hold your hand",
+		12 : "And when I touch you",
+		13 : "I feel happy inside",
+		14 : "It's such a feelin' that my love",
+		15 : "I can't hide",
+		16 : "I can't hide",
+		17 : "I can't hide",
+		18 : "Yeah, you got that somethin",
+		19 : "I think you'll understand",
+		20 : "When I say that somethin",
+		21 : "I want to hold your hand",
+		22 : "I want to hold your hand",
+		23 : "I want to hold your hand",
+		24 : "And when I touch you",
+		25 : "I feel happy inside",
+		26 : "It's such a feelin' that my love",
+		27 : "I can't hide",
+		28 : "I can't hide",
+		29 : "I can't hide",
+		30 : "Yeah, you got that somethin",
+		31 : "I think you'll understand",
+		32 : "When I feel that somethin",
+		33 : "I want to hold your hand",
+		34 : "I want to hold your hand",
+		35 : "I want to hold your hand",
+		36 : "I want to hold your hand"}
+
+
+a = int(input("Write any number"))
+for i in range(1,36):
+	if a < 37 and a > 0 
 
 
 
-b = input("write any number")
-for i in
-dic = {b: a}
-dic["first"]
-print(dic["first"])
+print(dic[int(input("Write any number"))])
